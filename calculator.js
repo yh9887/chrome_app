aeb28cad9262b4f49c3af959bb49f81ead21243f
yh9887 <gmlakddl987@naver.com>
2023-02-08 19:23:44 +0900

    첫번째 커밋

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const calcultor = {
+        add: function(a,b){
+            console.log(a+b)
+        },
+        minus: function(a,b){
+            console.log(a-b)
+        },
+        multiple: function(a,b){
+            console.log(a*b)
+        },
+        divide: function(a,b){
+            console.log(a/b)
+        },
+        squareRoot: function(a,b){
+            console.log(a**b)
+        }
+}
+calcultor.add(4,2)
+calcultor.minus(4,2)
+calcultor.multiple(4,2)
+calcultor.divide(4,2)
+calcultor.squareRoot(4,2)
