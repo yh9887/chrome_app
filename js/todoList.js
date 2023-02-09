93c501f8fccd5c53d180f29c2f6467d8aa4ec9f0
Younghee Hong <gmlakddl987@naver.com>
2023-02-10 01:14:50 +0900

    투두리스트 화면 표시

--- js/todoList.js
+++ js/todoList.js
@@ -2,10 +2,12 @@ const toDoForm = document.querySelector("#todo-form")
 const toDoInput = toDoForm.querySelector("input")
 const toDoList = document.querySelector("#todo-list")
 
-const toDos = [];
+const TODOS_KEY ="todos"
+
+let toDos = [];
 
 function saveToDos(){
-    localStorage.setItem("todos", JSON.stringfy(toDos))
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
 }
 
 function paintToDo(newTodo){
@@ -34,4 +36,23 @@ function handleTodoSubmit(event){
     saveToDos();
 }
 toDoForm.addEventListener('submit', handleTodoSubmit);
+
+
+console.log(saveToDos)
+const savedToDos = localStorage.getItem(TODOS_KEY)
+if(saveToDos !== null){
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    // parsedToDos.forEach((item) => console.log("heloo", item));
+
+    // paintToDo와 위 동일. 이 함수에서 다 해주기때문에 위처럼 복잡하게 할 필요 x
+    parsedToDos.forEach(paintToDo);
+}
+
+ // 둘 다 동일
+// (item) => console.log("heloo", item)
+
+// function sayHello(item){
+//     console.log("hello", item)
+// }
 
